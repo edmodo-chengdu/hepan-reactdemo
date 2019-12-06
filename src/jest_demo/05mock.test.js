03f1aeb978292339ccee93c2ba442942fa3fdf1e
hepan <pan@edmodo.com>
2019-12-06 16:03:48 +0800

    新增了mock

--- src/jest_demo/05mock.test.js
+++ src/jest_demo/05mock.test.js
@@ -14,7 +14,28 @@ describe("测试foreach函数", () => {
   expect(mockCallback.mock.calls[0][0]).toBe(0);
   expect(mockCallback.mock.calls[1][0]).toBe(1);
   expect(mockCallback.mock.results[1].value).toBe(2);
+  expect(mockCallback.mock.instances.length).toBe(2);
   test("true", () => {
     expect(true).toBe(true)
   })
+});
+describe("mock函数注入值", () => {
+  let mockDemo = jest.fn();
+  // 设置mock函数的返回值
+  mockDemo.mockReturnValueOnce(true).mockReturnValue(false);
+  console.log(mockDemo());
+  console.log(mockDemo());
+});
+test('mock function', () => {
+  const sum = jest.fn();
+  let val = sum(1, 2);
+  console.log(val);
+});
+test('mock function 的模拟实现', () => {
+  const sum3 = jest.fn().mockImplementation(
+    (a, b) => {
+      return a + b;
+    }
+  );
+  expect(sum3(4, 5)).toBe(9);
 });
